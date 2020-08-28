9c2fc637ac26fd5a0abab0bc73cfc22e0f1d80d4
shuwen he <645996795@qq.com>
2020-08-28 19:55:21 +0800

    feat: 0009.leetcode-isPalindrome-回文数

---
+++ 0009.leetcode-isPalindrome-回文数/main.go
@@ -0,0 +1,18 @@
+// 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+// 输入: 121
+// 输出: true
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	b := isPalindrome(121)
+	fmt.Println("b = ", b)
+}
+
+func isPalindrome(x int) bool {
+
+}
